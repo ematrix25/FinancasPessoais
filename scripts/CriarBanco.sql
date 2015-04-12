2fdf7e8276f5313689ec029f7ef75863198ea49f
ematrix25 <ematrix25@gmail.com>
2015-04-12 09:58:11 -0300

    CriarBanco
    Criado script inicial para gerar o banco

---
+++ scripts/CriarBanco.sql
@@ -0,0 +1,11 @@
+-- Database: financas
+
+-- DROP DATABASE financas;
+
+CREATE DATABASE financas
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
